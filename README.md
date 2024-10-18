8bc559098e10f52a695efa7e0c7f723f8f83ee7c
Álvaro Stagg <astagg@lumu.io>
2024-10-18 00:48:36 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# 4get
+
+Multithreading 4chan media downloader
+
+## Installation
+
+Download to your local machine and install dependencies using a virtualenv.
+Inside your local clone of 4get execute these commands:
+
+```shell
+python -m venv .venv
+# Linux/MacOS
+source .venv/bin/activate
+# Windows/Powershell
+.\.venv\Scripts\Activate.ps1
+```
+
+Then you can make `main.py` executable if you dont want to type `python` everytime:
+
+```shell
+chmod +x main.py
+```
+
+## Usage
+
+```shell
+usage: 4get [-h] [-t THREAD] [--version] [-o OUTPUT] [-v] [--ignore-formats IGNORE_FORMATS] [--keep-filename]
+
+4chan thread media downloader
+
+options:
+  -h, --help            show this help message and exit
+  -t THREAD, --thread THREAD
+                        4chan thread url
+  --version             print version and exit
+  -o OUTPUT, --output OUTPUT
+                        output directory path
+  -v, --verbose         show debug information
+  --ignore-formats IGNORE_FORMATS
+                        comma separated values of formats to ignore
+  --keep-filename       if set, if the media has a filename it will be used instead of 4chan timestamp
+```
+
+### Examples
+
+1. Download a thread without certain media types.
+
+```shell
+python main.py --ignore-formats "webm,gif,png" -t https://boards.4chan.org/\<board\>/thread/\<thread id\>
+```
+
+2. Download media into especific output directory.
+
+```shell
+python main.py -o /path/to/directory -t https://boards.4chan.org/\<board\>/thread/\<thread id\>
+```
+
+3. Keep original filename instead of 4chan timestamp.
+
+```shell
+python main.py --keep-filename /path/to/directory -t https://boards.4chan.org/\<board\>/thread/\<thread id\>
+```
+
+## License
+
+See [LICENSE](./LICENSE) file.
---
+++ main.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+
+import argparse
+import concurrent
+import concurrent.futures
+import logging
+import os
+import re
+import sys
+
+import requests
+
+VERSION = "1.0.0"
+
+
+def get_board_thread(board: str, id: int) -> dict:
+    resp = requests.get(f"https://a.4cdn.org/{board}/thread/{id}.json")
+    if resp.status_code != 200:
+        raise f"{resp.text}"
+    return resp.json()
+
+
+def save_thread_post(board: str, post: dict, output: str, keep_filename=False) -> None:
+    post_tim = str(post.get("tim"))
+    post_ext = post.get("ext")
+
+    local_output_path = (
+        os.path.join(output, post.get("filename") + post_ext)
+        if keep_filename
+        else os.path.join(output, post_tim + post_ext)
+    )
+    logging.debug(f"Downloading {post_tim + post_ext} into {local_output_path}")
+
+    resp = requests.get(f"https://i.4cdn.org/{board}/{post_tim}{post_ext}")
+    if resp.status_code != 200:
+        post_no = post.get("no")
+        logging.error(f"Could not download post no. {post_no}: {resp.text}")
+        return
+
+    with open(local_output_path, "wb") as file:
+        file.write(resp.content)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        prog="4get", description="4chan thread media downloader"
+    )
+
+    parser.add_argument("-t", "--thread", help="4chan thread url")
+    parser.add_argument("--version", action="store_true", help="print version and exit")
+    parser.add_argument(
+        "-o", "--output", dest="output", default=".", help="output directory path"
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="show debug information"
+    )
+    parser.add_argument(
+        "--ignore-formats",
+        default=[],
+        dest="ignore_formats",
+        help="comma separated values of formats to ignore",
+    )
+    parser.add_argument(
+        "--keep-filename",
+        action="store_true",
+        dest="keep_filename",
+        help="if set, if the media has a filename it will be used instead of 4chan timestamp",
+    )
+
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        format="[{asctime} {levelname}] {message}",
+        style="{",
+        datefmt="%Y-%m-%dT%H:%M:%S",
+        level=logging.DEBUG if args.verbose else logging.INFO,
+    )
+
+    if args.version:
+        print(f"4get v{VERSION}")
+        sys.exit(0)
+
+    if not os.path.exists(args.output):
+        logging.warning("Output directory does not exists, creating it")
+        os.makedirs(args.output)
+
+    expmatch = re.search(r"https://boards.4chan.org/(\w+)/thread/(\d+)", args.thread)
+    if expmatch:
+        board = expmatch.group(1)
+        thread_id = expmatch.group(2)
+    else:
+        logging.fatal("Could not extract board and thread if from provided URL")
+        sys.exit(1)
+
+    thread = None
+    try:
+        thread = get_board_thread(board, thread_id)
+    except Exception as e:
+        logging.error(f"Could not get thread: {e}")
+        sys.exit(1)
+
+    ignored_formats = [
+        fmt.strip().replace(".", "") for fmt in args.ignore_formats.strip().split(",")
+    ]
+
+    posts = [
+        p
+        for p in thread.get("posts")
+        if "filename" in p and p.get("ext").replace(".", "") not in ignored_formats
+    ]
+
+    if not posts:
+        logging.error("Thread does not have posts or no post match your criteria")
+        sys.exit(1)
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = {
+            executor.submit(
+                save_thread_post, board, post, args.output, args.keep_filename
+            ): post
+            for post in posts
+        }
+
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                rs = future.result()
+            except Exception as e:
+                logging.error(f"Post could not be downloaded: {e}")
